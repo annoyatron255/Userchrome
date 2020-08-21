f3480dcceb16a18d54d3884d38a8841ad839ded6
annoyatron255 <annoyatron255@gmail.com>
2020-08-21 17:23:04 -0500

    Switch UI to true transparency and remove old files

--- userChrome.js
+++
@@ -1,5 +0,0 @@
-gBrowser.tabContainer.addEventListener('dblclick', function(event) {
-	if (event.target.localName == 'tab' && event.button == 0) {
-		document.getElementById('urlbar').focus();
-	}
-});
